d79fa0079220c54b424f0274c3c4642e772c4538
SilverGrost <silvergrost@silcomsoft.ru>
2021-10-31 21:27:58 +0300

    Сделал layout

--- app/src/main/java/ru/silcomsoft/jpgtopng/view/MainActivity.kt
+++ app/src/main/java/ru/silcomsoft/jpgtopng/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.silcomsoft.jpgtopng
+package ru.silcomsoft.jpgtopng.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.silcomsoft.jpgtopng.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
